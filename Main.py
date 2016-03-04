d0ee8fee8bd0a915eab604f9f5ee01eb37405bd0
ryanlpeterson <ryanlpeterson@hotmail.com>
2016-03-03 21:03:37 -0500

    Update Main.py
    Updated to new class names

--- Main.py
+++ Main.py
@@ -4,15 +4,15 @@
 
 import sys
 
-from EntertainmentCollection import EntertainmentCollection
-from EntertainmentItem import EntertainmentItem
+from Table import Table
+from Record import Record
 
-entertainment_collection = EntertainmentCollection("The Fox's Collection")
+table = Table("The Fox's Collection")
 
-entertainment_collection.add_item(EntertainmentItem("Saga", 30, 30))
-entertainment_collection.add_item(EntertainmentItem("The Walking Dead", 36, 144))
-entertainment_collection.add_item(EntertainmentItem("Parks and Recreation", 7, 7))
-entertainment_collection.add_item(EntertainmentItem("30 Rock", 4, 7))
+table.add_item(Record("Saga", 30, 30))
+table.add_item(Record("The Walking Dead", 36, 144))
+table.add_item(Record("Parks and Recreation", 7, 7))
+table.add_item(Record("30 Rock", 4, 7))
 
 command = input("What would you like to do? (add, delete, write, open, exit): ")
 while True:
@@ -21,19 +21,19 @@ while True:
         name = input("Name: ")
         amount_completed = int(input("Amount Completed: "))
         length = int(input("Length: "))
-        entertainment_collection.add_item(EntertainmentItem(name, amount_completed, length))
+        table.add_item(Record(name, amount_completed, length))
     elif command == "delete":
         name = input("Name: ")
-        entertainment_collection.delete_item(name)
+        table.delete_item(name)
     elif command == "write":
         file_name = input("File name (example.txt): ")
-        entertainment_collection.write_to_file(file_name)
+        table.write_to_file(file_name)
     elif command == "open":
         while True:
             save = input("Would you like to save changes? (y/n): ")
             if save == "y":
                 file_name = input("File name (example.txt): ")
-                entertainment_collection.write_to_file(file_name)
+                table.write_to_file(file_name)
                 break
             elif save == "n":
                 break
@@ -41,13 +41,13 @@ while True:
                 print("Sorry I don't understand.")
 
         file_name = input("File name (example.txt): ")
-        entertainment_collection = EntertainmentCollection.open_file(file_name) #for some reason combining
+        table = Table.open_file(file_name) #for some reason combining
     elif command == "exit":
         while True:
             save = input("Would you like to save changes? (y/n): ")
             if save == "y":
                 file_name = input("File name (example.txt): ")
-                entertainment_collection.write_to_file(file_name)
+                table.write_to_file(file_name)
                 break
             elif save == "n":
                 break
